82d4397c7f91e2732577667a6070085c57f1f726
Julian Cheal <jcheal@redhat.com>
2018-10-09 16:34:28 +0100

    Adding Ruby v2.5 runtime

---
+++ resources/vars.rb
@@ -0,0 +1,10 @@
+def main(args)
+  {
+       "api_host": ENV['__OW_API_HOST'],
+       "api_key": ENV['__OW_API_KEY'],
+       "namespace": ENV['__OW_NAMESPACE'],
+       "action_name": ENV['__OW_ACTION_NAME'],
+       "activation_id": ENV['__OW_ACTIVATION_ID'],
+       "deadline": ENV['__OW_DEADLINE']
+  }
+end
